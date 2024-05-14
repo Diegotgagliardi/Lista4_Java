d172f00520d0d19de1f7ddbfb35b0060d8d192a8
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-05-14 18:59:43 -0300

    cernario 6 inicio

---
+++ Cenario6/src/br/edu/up/model/Pessoa.java
@@ -0,0 +1,21 @@
+package br.edu.up.model;
+
+public class Pessoa {
+    private String nome;
+    private String RG;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRG() {
+        return RG;
+    }
+    public void setRG(String rG) {
+        RG = rG;
+    }
+
+    
+}
